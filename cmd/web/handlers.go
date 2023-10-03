3002f74ae9f0e93d2feb3125e7a12ba3af155552
Lazar <baldur059@gmail.com>
2023-10-03 22:21:08 +0200

    execute insert statement

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -59,5 +59,16 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Creating a new snippet..."))
+	title := "0 snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	// prosljeđivanje podataka ka bazi
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
