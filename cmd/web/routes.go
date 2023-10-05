0f52c547bd1a7a8362437ef98a71e85f757fb29e
Lazar <lazar.mrkic@sigmoid.rs>
2023-10-05 11:22:45 +0200

    set security headers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,21 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+// request-ovi će biti proslijeđeni na sledeći šablon "secureHeaders → servemux → application handler"
+// kada se vrati zadnji "handler" u lancu, onda se kontrola vraća nazad - u kontra smjeru
+// secureHeaders → servemux → application handler → servemux → secureHeaders
+/*
+	func myMiddleware(next http.Handler) http.Handler {
+    	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Any code here will execute on the way down the chain.
+			next.ServeHTTP(w, r)
+			// Any code here will execute on the way back up the chain.
+    	})
+	}
+*/
+
+// povratna vrijednost "routes()" metode sada treba da bude "http.Handler"
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	// kreiranje "file" servera - za fajlove iz "ui/static" foldera
@@ -16,5 +30,23 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	return secureHeaders(mux)
 }
+
+// ukoliko se odradi "return" prije narednog poziva "next.ServeHTTP()" - onda se prekida lanac izvršavanja
+// kontrola se vraća uzvodno
+/*
+	func myMiddleware(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// If the user isn't authorized, send a 403 Forbidden status and
+			// return to stop executing the chain.
+			if !isAuthorized(r) {
+				w.WriteHeader(http.StatusForbidden)
+				return
+			}
+
+			// Otherwise, call the next handler in the chain.
+			next.ServeHTTP(w, r)
+		})
+	}
+*/
