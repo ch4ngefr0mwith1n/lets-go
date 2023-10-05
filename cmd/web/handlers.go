23e2e878cdf636acead855fd47c4d21b332e74fb
Lazar <lazar.mrkic@sigmoid.rs>
2023-10-05 15:01:56 +0200

    add validation for create post form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"snippetbox.lazarmrkic.com/internal/models"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 )
 
 // "home" handler će postati metoda "application" struct-a:
@@ -91,6 +93,32 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// unutar ove mapeće biti sve potencijalne greške prilikom validacije polja
+	fieldErrors := make(map[string]string)
+
+	// odličan blog post koji sadrži generalne metode za validaciju:
+	// https://www.alexedwards.net/blog/validation-snippets-for-go#email-validation
+	// provjera:
+	if strings.TrimSpace(title) == "" {
+		fieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+
+	if strings.TrimSpace(content) == "" {
+		fieldErrors["content"] = "This field cannot be blank"
+	}
+
+	if expires != 1 && expires != 7 && expires != 365 {
+		fieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+
+	// ukoliko neke od grešaka postoje, treba ih baciti u "plain text HTTP response" i vratiti iz handler-a
+	if len(fieldErrors) > 0 {
+		fmt.Fprint(w, fieldErrors)
 		return
 	}
 
