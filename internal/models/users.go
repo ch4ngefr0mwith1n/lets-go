a1a5149c94e23e111ba1e9f7ce01520b0c83dc1e
Lazar <baldur059@gmail.com>
2023-10-07 06:11:25 +0200

    Äcreate a users modelÄ

---
+++ internal/models/users.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Created        time.Time
+}
+
+// "UserModel" struct omotava "connection pool"
+// biće proslijeđen u "handlers" kao zavisnost
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (m *UserModel) Insert(name string, email string, password string) error {
+	return nil
+}
+
+func (m *UserModel) Authenticate(email string, password string) (int, error) {
+	return 0, nil
+}
+
+func (m *UserModel) Exists(id int) (bool, error) {
+	return false, nil
+}
