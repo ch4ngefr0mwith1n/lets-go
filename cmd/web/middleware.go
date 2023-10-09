0fd504e1f2cec937eab041ac4b25e3caafb7be04
Lazar <lazar.mrkic@sigmoid.rs>
2023-10-09 15:57:12 +0200

    current progress

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/justinas/nosurf"
 	"net/http"
 )
 
@@ -69,4 +70,22 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 		w.Header().Add("Cache-Control", "no-store")
 		next.ServeHTTP(w, r)
 	})
+}
+
+// kako bismo izbjegli CSRF, koristićemo "nosurf" paket
+// koristi se "double-submit cookie" pristup
+// prvo se generiše "random CSRF token" i šalje korisniku unutar "CSRF cookie"-a
+// nakon toga, ovaj "CSRF token" se dodaje u svako HTML polje koje je ranjivo na CSRF
+// pri slanju forme, koristi se neki "middleware" - koji provjerava da li se skrivena vrijednost u HTML polju i vrijednost "cookie"-a poklapaju
+
+// ova "middleware" funkcija koristi prilagođeni CSRF cookie, unutar kog su postavljene vrijednosti na tri atributa
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: false,
+		Path:     "",
+		Secure:   false,
+	})
+
+	return csrfHandler
 }
